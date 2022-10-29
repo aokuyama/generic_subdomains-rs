ac7ee9fd5288b78465ff035499591d8b5e3884b1
aokuyama <ao980s@gmail.com>
2022-10-29 09:16:24 +0900

    add file download util

---
+++ src/file/mod.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io;
+
+#[derive(Debug)]
+pub enum Error {
+    ReqwestError(String),
+    StdError(String),
+}
+
+pub async fn download(url: &str, local_path: &str) -> Result<(), Error>{
+    let response = match reqwest::get(url).await {
+        Ok(response) => response,
+        Err(err) => return Err(Error::ReqwestError(err.to_string())),
+    };
+    let bytes = match response.bytes().await {
+        Ok(bytes) => bytes,
+        Err(err) => return Err(Error::ReqwestError(err.to_string())),
+    };
+    let mut out = match File::create(local_path) {
+        Ok(bytes) => bytes,
+        Err(err) => return Err(Error::StdError(err.to_string())),
+    };
+    match io::copy(&mut bytes.as_ref(), &mut out) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Error::StdError(err.to_string())),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use generic_subdomains::file::download;
+
+#[tokio::main]
+async fn main() {
+    match download("https://doc.rust-jp.rs", "test.html").await {
+        Ok(_) => (),
+        Err(e) => panic!("{:?}", e),
+    };
 }
